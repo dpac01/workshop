f0c7883c0f29e0e8b5fd83a5bf3c24a703e89fb7
Dpac <dpac01@gmail.com>
2016-01-30 16:22:47 -0700

    initial commit

---
+++ conditional.py
@@ -0,0 +1,10 @@
+x = int(input("x:"))
+y = int(input("y:"))
+if not(x<10):
+    print("true " +str(x))
+elif x<5 and y > 12:
+    print("more true")
+else:
+    print("false")
+    
+    
---
+++ iteration.py
@@ -0,0 +1,11 @@
+books = ["HP","LOTR","50S"]
+nums = range(100)
+
+for book in books:
+    print(book)
+
+for i in nums:
+    print(i)
+
+for book1 in books:
+    print(books.index(book1))
